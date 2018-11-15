e1a5fc7e0a12ff7da4b57b200cc1e132e4a09c93
weijia.cheng <weijia.cheng@beibei.com>
2018-11-15 21:05:39 +0800

    add: 手势监听

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,46 @@
 import '../css/main.css'
 require("expose-loader?$!jquery");
 
+// 禁止页面下滑
+document.body.addEventListener('touchmove',function(e){
+    e.preventDefault()
+  },{ passive:false })
+
+function GetSlideAngle(dx, dy) { 
+    return Math.atan2(dy, dx) * 180 / Math.PI; 
+} 
+
+//根据起点和终点返回方向 1：向上，2：向下，3：向左，4：向右,0：未滑动 
+function GetSlideDirection(startX, startY, endX, endY) { 
+  var dy = endY - startY; 
+  var dx = endX - startX;
+  var dyRem = Math.floor(dy/46.875);
+  var dxRem = Math.floor(dx/46.875/4);
+
+  var resultObj = {
+    direction: 0,
+    dyRem: dyRem,
+    dxRem: dxRem
+  };
+  //如果滑动距离太短 
+  if(Math.abs(dx) < 2 && Math.abs(dy) < 2) { 
+      return resultObj; 
+  } 
+  var angle = GetSlideAngle(dx, dy); 
+  if(angle >= -45 && angle < 45) { 
+    resultObj.direction = 4; 
+  }else if (angle >= 45 && angle < 135) { 
+    resultObj.direction = 1; 
+  }else if (angle >= -135 && angle < -45) { 
+    resultObj.direction = 2; 
+  } 
+  else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) { 
+    resultObj.direction = 3; 
+  } 
+  return resultObj; 
+} 
+
+
 var classicBtn = document.getElementsByClassName('classic-model')[0];
 var puzzlesBtn = document.getElementsByClassName('puzzles-model')[0];
 
@@ -230,7 +270,7 @@ function ClassicGame () {
         ]
     ];
     // 随机颜色
-    this.colors = ['#92cfe4', '#e492dc', '#92e493', '#ff9000'];
+    this.colors = ['#439e95', '#eda83f', '#226fb2', '#e23e34', '#85bc3c', '#8c2e89'];
 
     
     // 显示游戏区域所有网格
@@ -243,11 +283,12 @@ function ClassicGame () {
     // 掉落
     // this.autoMove();
     var that = this;
+    that.isDown = '';
     function go() {
         that.init();
 		that.bindEvents();
-        var isDown = that.autoMove();
-        $.when(isDown).then(function() {
+        that.isDown = that.autoMove();
+        $.when(that.isDown).then(function() {
             var nextBlocks = Util.getElementByAttr('block','next','true');
             for(var i = 0; i < nextBlocks.length; i++){
                 nextBlocks[i].parentNode.removeChild(nextBlocks[i]); 
@@ -255,7 +296,32 @@ function ClassicGame () {
 			go();
 		})
 	}
-	go();
+    go();
+    console.log('isDown', that.isDown);
+    
+    //滑动处理 
+    var startX, startY; 
+    document.addEventListener('touchstart',function (ev) { 
+    startX = ev.touches[0].pageX; 
+    startY = ev.touches[0].pageY;   
+    }, false); 
+
+    document.addEventListener('touchend',function (ev) { 
+        var endX, endY; 
+        endX = ev.changedTouches[0].pageX; 
+        endY = ev.changedTouches[0].pageY; 
+        var resultObj = GetSlideDirection(startX, startY, endX, endY); 
+        if (resultObj.direction === 1 && resultObj.dyRem > 15) {
+            that.moveDownFaster(that, 'touch');
+        } else if (resultObj.direction === 2) {
+            that.moveRotate(that);
+        } else if (resultObj.direction === 3) {
+            that.moveLeft(that, resultObj.dxRem);
+        } else if (resultObj.direction === 4) {
+            that.moveRight(that, resultObj.dxRem);
+        }
+        
+      }, false);
 }
 
 ClassicGame.prototype.init = function() {
@@ -272,8 +338,8 @@ ClassicGame.prototype.init = function() {
 }
 
 ClassicGame.prototype.initGrids = function() {
-    for(var i = 0; i < 18; i++) {
-		for(var j = 0; j < 10; j++) {
+    for(var i = 0; i < 19; i++) {
+		for(var j = 0; j < 15; j++) {
             var grid = document.createElement("div");
             grid.style.cssText = "top:"+i+"rem;left:"+j+"rem;";
             grid.className = 'grid';
@@ -285,7 +351,7 @@ ClassicGame.prototype.initGrids = function() {
 ClassicGame.prototype.initRandom = function() {
     this.r3 = Math.floor(Math.random() * 6);
     this.r4 = Math.floor(Math.random() * 4);
-    this.r5 = Math.floor(Math.random() * 4);
+    this.r5 = Math.floor(Math.random() * 6);
     
     // this.r3 = 1;
     // this.r4 = 0;
@@ -307,7 +373,7 @@ ClassicGame.prototype.initBlock = function() {
 ClassicGame.prototype.initNext = function() {
     for (var i = 0; i < 4; i++) {
         var next = document.createElement("div");
-        var top = this.tetris[this.r3][this.r4][i][1] + 3;
+        var top = this.tetris[this.r3][this.r4][i][1] + 2.5;
         var left = this.tetris[this.r3][this.r4][i][0] - 4;
         next.style.cssText = "top:"+top+"rem;left:"+left+"rem;background:"+this.colors[this.r5];
         next.className = 'block';
@@ -336,6 +402,9 @@ ClassicGame.prototype.bindEvents = function() {
 
     $(".btn-down").on({
         touchstart: function(e) {
+            if(that.timeOutEvent) {
+                clearTimeout(that.timeOutEvent);
+            }
             that.longClick = 0;
             that.timeOutEvent =setTimeout(function(){
                 that.longClick=1;
@@ -377,7 +446,7 @@ ClassicGame.prototype.canMoveLeftOrRight = function(i, x) {
     var left = this.tetris[this.r1][this.r2][i][0] + x; // 下一个位置的left
     var blockPosition = top+'rem'+left+'rem';
 
-    if (left>= 0 && left<= 9) {
+    if (left>= 0 && left<= 14) {
         if(this.blockArea.indexOf(blockPosition) == -1) {
             return true;
         } else {
@@ -388,15 +457,19 @@ ClassicGame.prototype.canMoveLeftOrRight = function(i, x) {
     }
 }
 
-ClassicGame.prototype.moveLeft = function(that) {
+ClassicGame.prototype.moveLeft = function(that, dxRem) {
     // this 指向按钮
-    var canML0 = that.canMoveLeftOrRight(0, -1);
-    var canML1 = that.canMoveLeftOrRight(1, -1);
-    var canML2 = that.canMoveLeftOrRight(2, -1);
-    var canML3 = that.canMoveLeftOrRight(3, -1);
+    var num = -1;
+    if (dxRem) {
+        num = dxRem;
+    }
+    var canML0 = that.canMoveLeftOrRight(0, num);
+    var canML1 = that.canMoveLeftOrRight(1, num);
+    var canML2 = that.canMoveLeftOrRight(2, num);
+    var canML3 = that.canMoveLeftOrRight(3, num);
 
     if (canML0 && canML1 && canML2 && canML3) {
-        that.x -= 1;
+        that.x += num;
         that.updateTetris();
 
         var moveBlocks = Util.getElementByAttr('block','move','true');
@@ -406,14 +479,18 @@ ClassicGame.prototype.moveLeft = function(that) {
     }
 
 }
-ClassicGame.prototype.moveRight = function(that) {
-    var canMR0 = this.canMoveLeftOrRight(0, 1);
-    var canMR1 = this.canMoveLeftOrRight(1, 1);
-    var canMR2 = this.canMoveLeftOrRight(2, 1);
-    var canMR3 = this.canMoveLeftOrRight(3, 1);
+ClassicGame.prototype.moveRight = function(that, dxRem) {
+    var num = 1;
+    if (dxRem) {
+        num = dxRem;
+    }
+    var canMR0 = this.canMoveLeftOrRight(0, num);
+    var canMR1 = this.canMoveLeftOrRight(1, num);
+    var canMR2 = this.canMoveLeftOrRight(2, num);
+    var canMR3 = this.canMoveLeftOrRight(3, num);
 
     if (canMR0 && canMR1 && canMR2 && canMR3) {
-        that.x += 1;
+        that.x += num;
         that.updateTetris();
 
         var moveBlocks = Util.getElementByAttr('block','move','true');
@@ -429,7 +506,7 @@ ClassicGame.prototype.canMoveRotate = function(i) {
     var left = this.tetris[this.r1][this.r2][i][0];
     var blockPosition = top+'rem'+left+'rem';
 
-    if (left>= 0 && left<= 9 && top <= 17) {
+    if (left>= 0 && left<= 14 && top <= 18) {
         if(this.blockArea.indexOf(blockPosition) == -1) {
             return true;
         } else {
@@ -465,7 +542,7 @@ ClassicGame.prototype.canMoveDown = function(i, y) {
     var left = this.tetris[this.r1][this.r2][i][0];
     var blockPosition = top+'rem'+left+'rem';
 
-    if (top <= 17) {
+    if (top <= 18) {
         if(this.blockArea.indexOf(blockPosition) == -1) {
             return true;
         } else {
@@ -543,21 +620,29 @@ ClassicGame.prototype.autoMove = function() {
     })
 }
 
-ClassicGame.prototype.moveDownFaster = function(that) {
-	that.speed = 50
+ClassicGame.prototype.moveDownFaster = function(that, type) {
+    // todo
+    that.speed = 50;
+    if (type == 'touch') {
+        $.when(that.isDown).then(function() {
+            that.speed = 600;
+        })
+    }
+   
+	
 }
 
 ClassicGame.prototype.isClean = function() {
     var that = this;
     $('.block[move=true]').each(function() {
-        var style = $(this).attr("style"); // "top: 17rem; left: 5rem;"
+        var style = $(this).attr("style");
         var top = style.split(';')[0].replace(/[^0-9]/ig,"") + 'rem';
         var comTop = style.split(';')[0].replace(/[^0-9]/ig,"");
         var left = style.split(';')[1].replace(/[^0-9]/ig,"") + 'rem';
         that.blockArea.push(top+''+left);
         $(this).attr('top', top);
         $(this).attr('move', false);
-        if($('.block[top=' + top + ']').length == 10) {
+        if($('.block[top=' + top + ']').length == 15) {
             that.score += 100;
             that.remove = true;
             var r0 = that.removes(0, top);
@@ -569,8 +654,13 @@ ClassicGame.prototype.isClean = function() {
 			var r6 = that.removes(6, top);
 			var r7 = that.removes(7, top);
 			var r8 = that.removes(8, top);
-			var r9 = that.removes(9, top);
-            $.when(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9).then(function() {
+            var r9 = that.removes(9, top);
+            var r10 = that.removes(10, top);
+            var r11 = that.removes(11, top);
+            var r12 = that.removes(12, top);
+            var r13 = that.removes(13, top);
+            var r14 = that.removes(14, top);
+            $.when(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14).then(function() {
 				$('.score').text(that.score);
 				$('.block[move=false]').each(function() {
                     var style1 = $(this).attr("style");
